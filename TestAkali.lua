b64e76d9c6eec47bbb932a81c3f8eff1ae5d8a28
Toshibiotro <ademdark@live.co.uk>
2016-06-28 02:21:53 +0000

    Update TestAkali.lua

--- TestAkali.lua
+++ TestAkali.lua
@@ -1,14 +1,12 @@
 if GetObjectName(GetMyHero()) ~= "Akali" then return end
 
-local ver = "0.04"
+local ver = "0.07"
 
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         print("New version found! " .. data)
         print("Downloading update, please wait...")
         DownloadFileAsync("https://raw.githubusercontent.com/Toshibiotro/stuff/master/CustomAkali.lua", SCRIPT_PATH .. "CustomAkali.lua", function() print("Update Complete, please 2x F6!") return end)
-    else
-        print("No updates found!")
     end
 end
 
@@ -16,7 +14,6 @@ GetWebResultAsync("https://raw.githubusercontent.com/Toshibiotro/stuff/master/Cu
 
 if FileExist(COMMON_PATH.."MixLib.lua") then
  require('MixLib')
- Mix:LoadPred()
 else
  PrintChat("MixLib not found. Please wait for download.")
  DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/NEET-Scripts/master/MixLib.lua", COMMON_PATH.."MixLib.lua", function() PrintChat("Downloaded MixLib. Please 2x F6!") return end)
@@ -37,6 +34,13 @@ AkaliMenu.LaneClear:Boolean("Q", "Use Q", true)
 AkaliMenu.LaneClear:Boolean("E", "Use E", true)
 AkaliMenu.LaneClear:Slider("EnergyManager", "Min Energy to LaneClear",100,0,200,10)
 
+AkaliMenu:SubMenu("LastHit","LastHit")
+AkaliMenu.LastHit:Boolean("QLH", "Use Q", true)
+AkaliMenu.LastHit:Boolean("ELH", "Use E", true)
+AkaliMenu.LastHit:Slider("LHEnergyManager", "Min Energy to Use Last Hit",0,0,200,10)
+AkaliMenu.LastHit:Boolean("ALHQ", "Auto Last Hit", true)
+AkaliMenu.LastHit:Slider("ALHEnergyManager", "Min Energy to Use Auto Last Hit",0,0,200,10)
+
 AkaliMenu:SubMenu("KillSteal", "KillSteal")
 AkaliMenu.KillSteal:Boolean("KSQ", "KillSteal with Q", true)
 AkaliMenu.KillSteal:Boolean("KSE", "KillSteal with E", true)
@@ -50,6 +54,19 @@ AkaliMenu.Misc:Boolean("AutoW", "UseAutoW", true)
 AkaliMenu.Misc:Slider("AutoWP", "Percent Health for Auto W",20,5,90,2)
 AkaliMenu.Misc:Boolean("AutoWE", "Use Auto W on X Enemies", true)
 AkaliMenu.Misc:Slider("AutoWX", "X Enemies to Cast AutoW",3,1,5,1)
+AkaliMenu.Misc:Boolean("AutoI", "Auto Ignite", true)
+
+AkaliMenu:SubMenu("Draw", "Drawings")
+AkaliMenu.Draw:Boolean("DAA", "Draw AA Range", true)
+AkaliMenu.Draw:Boolean("DQ", "Draw Q Range", true)
+AkaliMenu.Draw:Boolean("DW", "Draw W Range", true)
+AkaliMenu.Draw:Boolean("DDW", "Draw W Position", true)
+AkaliMenu.Draw:Boolean("DE", "Draw E Range", true)
+AkaliMenu.Draw:Boolean("DR", "Draw R Range", true)
+AkaliMenu.Draw:Boolean("DrawK", "Draw if Target is Killable", true)
+
+AkaliMenu:SubMenu("Escape", "Escape, Hold G")
+AkaliMenu.Escape:Boolean("ESCR", "Use R", true)
 
 AkaliMenu:SubMenu("SkinChanger", "SkinChanger")
 
@@ -58,6 +75,7 @@ AkaliMenu.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[m
 AkaliMenu.SkinChanger.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
 
 local nextAttack = 0
+local AnimationQ = 1
 
 OnTick(function ()
 
@@ -74,8 +92,7 @@ OnTick(function ()
 		if GetCurrentMana(myHero) >= AkaliMenu.Combo.ComboEnergyManager:Value() then
 			if AkaliMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 600) then
 					CastTargetSpell(target, _Q)
-  
-        end
+        	end
 
         	if GetCurrentMana(myHero) >= AkaliMenu.Combo.ComboEnergyManager:Value() then        
          		if GetTickCount() > nextAttack then	
@@ -85,7 +102,7 @@ OnTick(function ()
 				end
 			end
 	
-			if GetDistance(target, myHero) >= 240 then
+			if GetDistance(target, myHero) >= 325 or GetCurrentMana(myHero) < 40 then
 				if GetTickCount() > nextAttack then	
 					if AkaliMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 700) then
 						CastTargetSpell(target, _R)
@@ -123,23 +140,61 @@ OnTick(function ()
 			end
 		end
 	end
+
+	if Mix:Mode() == "LastHit" then
+	
+		for _,closeminion in pairs(minionManager.objects) do
+			if GetCurrentMana(myHero) > AkaliMenu.LastHit.LHEnergyManager:Value() then
+				if AkaliMenu.LastHit.QLH:Value() and Ready(_Q) and ValidTarget(closeminion, 600) then
+					if GetDistance(closeminion, myHero) > 325 then	
+						if GetCurrentHP(closeminion) < CalcDamage(myHero, closeminion, 0, 15 + 20 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.4) then
+							CastTargetSpell(closeminion, _Q) 
+						end
+					end
+				end
+			end	
+			
+			if GetCurrentMana(myHero) > AkaliMenu.LastHit.LHEnergyManager:Value() then
+				if GetTickCount() > nextAttack then
+					if AkaliMenu.LastHit.ELH:Value() and Ready(_E) and ValidTarget(closeminion, 325) then
+						if GetCurrentHP(closeminion) < CalcDamage(myHero, closeminion, 0, 5 + 25 * GetCastLevel(myHero,_E) + GetBonusAP(myHero) * 0.4 + (myHero.totalDamage) * 0.6) then
+							CastSpell(_E)
+						end	
+					end
+				end
+			end
+		end	
+	end
+	
+	--AutoLastHit
+	for _,closeminion in pairs(minionManager.objects) do
+		if not KeyIsDown(32) then
+			if GetCurrentMana(myHero) > AkaliMenu.LastHit.ALHEnergyManager:Value() and Ready(_Q) and ValidTarget(closeminion, 600) then
+				if GetTickCount() > nextAttack then
+					if GetCurrentHP(closeminion) < CalcDamage(myHero, closeminion, 0, 15 + 20 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.4) then
+						CastTargetSpell(closeminion, _Q)
+					end
+				end
+			end
+		end
+	end	
 	
 	--Killsteal
 	for _, enemy in pairs(GetEnemyHeroes()) do
 		if AkaliMenu.KillSteal.KSQ:Value() and Ready(_Q) and ValidTarget(enemy, 600) then
-			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 35 + 20 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.4) then
-	           	CastTargetSpell(enemy , _Q)
+			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 15 + 20 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.4) then
+	           		CastTargetSpell(enemy , _Q)
 			end
 		end
 	
 		if AkaliMenu.KillSteal.KSR:Value() and Ready(_R) and ValidTarget(enemy, 700) then
-			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 100 + 75 * GetCastLevel(myHero,_R) + GetBonusAP(myHero) * 0.5) then
+			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 25 + 75 * GetCastLevel(myHero,_R) + GetBonusAP(myHero) * 0.5) then
 				CastTargetSpell(enemy , _R)
 			end
 		end
 	
 		if AkaliMenu.KillSteal.KSE:Value() and Ready(_E) and ValidTarget(enemy, 325) then
-			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 30 + 25 * GetCastLevel(myHero,_E) + GetBonusAP(myHero) * 0.4 + (myHero.totalDamage) * 0.6) then
+			if GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 5 + 25 * GetCastLevel(myHero,_E) + GetBonusAP(myHero) * 0.4 + (myHero.totalDamage) * 0.6) then
 				CastSpell(_E)
 			end
 		end
@@ -161,12 +216,78 @@ OnTick(function ()
 	if AkaliMenu.Misc.AutoW:Value() and Ready(_W) and EnemiesAround(myHeroPos(), 1000) >= 1 and (EnemiesAround(myHeroPos(), 1000) >= AkaliMenu.Misc.AutoWX:Value() or GetPercentHP(myHero) <= AkaliMenu.Misc.AutoWP:Value()) then
 		CastSkillShot(_W, myHeroPos())
 	end
+	
+	--Escape
+	if KeyIsDown(71) then 
+			MoveToXYZ(GetMousePos())
+		for _,closeminion in pairs(minionManager.objects) do	
+			if AkaliMenu.Escape.ESCR:Value() and Ready(_R) and ValidTarget(closeminion,700) then	
+				if EnemiesAround(closeminion, 700) < 1 then
+						CastTargetSpell(closeminion, _R)
+				end	
+			end
+		end	
+	end
+	
+	--AutoIgnite
+	for _, enemy in pairs(GetEnemyHeroes()) do
+		if GetCastName(myHero, SUMMONER_1):lower():find("summonerdot") then
+			if AkaliMenu.Misc.AutoI:Value() and Ready(SUMMONER_1) and ValidTarget(enemy, 600) then
+				local IDamage = (50 + (20 * GetLevel(myHero)))
+				if GetCurrentHP(enemy) < IDamage then
+				CastTargetSpell(enemy, SUMMONER_1)
+				elseif GetCastName(myHero, SUMMONER_2):lower():find("summonerdot") then
+					if AkaliMenu.Misc.AutoI:Value() and Ready(SUMMONER_2) and ValidTarget(enemy, 600) then
+						if GetCurrentHP(enemy) < IDamage then
+							CastTargetSpell(enemy, SUMMONER_2)
+						end
+					end
+				end
+			end
+		end	
+	end
 end)
 
 OnProcessSpell(function(unit,spellProc)
 	if unit.isMe and spellProc.name:lower():find("attack") and spellProc.target.isHero then
-		nextAttack = GetTickCount() + spellProc.windUpTime * .001
+		nextAttack = GetTickCount() + spellProc.windUpTime * 1000
 	end
+	
+		if unit.isMe and spellProc.name:lower():find("akalimota") and spellProc.target.isminion then
+			AnimationQ = GetTickCount() + spellProc.windUpTime * 1000
+		end
 end)
 
+OnDraw(function(myHero)
+	local pos = GetOrigin(myHero)
+	local mpos = GetMousePos()
+	if AkaliMenu.Draw.DQ:Value() then DrawCircle(pos, 600, 1, 25, GoS.White) end
+	if AkaliMenu.Draw.DAA:Value() then DrawCircle(pos, 125, 1, 25, GoS.Green) end
+	if AkaliMenu.Draw.DE:Value() then DrawCircle(pos, 325, 1, 25, GoS.Yellow) end
+	if AkaliMenu.Draw.DR:Value() then DrawCircle(pos, 700, 1, 25, GoS.Cyan) end
+	if AkaliMenu.Draw.DW:Value() then DrawCircle(pos, 700, 1, 25, GoS.Blue) end
+	if AkaliMenu.Draw.DDW:Value() then DrawCircle(mpos, 400, 1, 25, GoS.Red) end
+
+	for _, enemy in pairs(GetEnemyHeroes()) do
+		local epos = GetOrigin(enemy)
+		local drawpos = WorldToScreen(1,epos.x, epos.y, epos.z)
+		local QDamage = CalcDamage(myHero, enemy, 0, 15 + 20 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.4)
+		local QPDamage = CalcDamage(myHero, enemy, 0, 20 + 25 * GetCastLevel(myHero,_Q) + GetBonusAP(myHero) * 0.5)
+		local EDamage = CalcDamage(myHero, enemy, 0, 5 + 25 * GetCastLevel(myHero,_E) + GetBonusAP(myHero) * 0.4 + (myHero.totalDamage) * 0.6)
+		local RDamage = CalcDamage(myHero, enemy, 0, 25 + 75 * GetCastLevel(myHero,_R) + GetBonusAP(myHero) * 0.5)
+		local HTGBDamage = CalcDamage(myHero, enemy, 0, 250 + GetBonusAP(myHero) * 0.3)
+		if AkaliMenu.Draw.DrawK:Value() then
+			if enemy.valid then
+				if Ready(_Q) and Ready(_E) and Ready (_R) and Ready(GetItemSlot(myHero, 3146)) then
+					if GetCurrentHP(enemy) < QDamage + QPDamage + EDamage + (RDamage * GetSpellData(myHero,_R).ammo) + HTGBDamage then
+						DrawText("Killable", 30, drawpos.x-60, drawpos.y, GoS.White)
+						else
+						DrawText("Not Killable", 30, drawpos.x-60, drawpos.y, GoS.White)
+					end
+				end
+			end
+		end	
+	end	
+end)
+	
 print("Thank You For Using Custom Akali, Have Fun :D")
